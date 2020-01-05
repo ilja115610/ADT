85c2cfa62f15bc7552247e6a8638cefc4b005b48
ilja115610 <57315435+ilja115610@users.noreply.github.com>
2020-01-05 16:25:29 +0200

    final change

--- src/com/company/Counter.java
+++ src/com/company/Counter.java
@@ -1,9 +1,23 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Counter {
 
    private String name;
     private int counter;
+
+
+    public int [] array = new int [2];
+    public int [] arr;
+
+
+
+    public List<String> newList = null;
+
+    public ArrayList<Integer> newLis;
+
 
     public Counter(String str) {
         this.name = str;
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,59 +1,23 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
+
+
+
+
+
 
     public static void main(String[] args) {
 
-    HashTable<String, Integer> newTable = new HashTable<>(13);
-    newTable.insert("Kolja",12345);
-    newTable.insert("Vasja",45678);
-    newTable.insert("Anna",987454);
-
-    //newTable.displayAll();
-    System.out.print(newTable.findByName("Anna"));
+        Main mn = new Main();
 
 
-        //Counter newCount = new Counter ("times");
-        //newCount.increment();
-        //System.out.println(newCount.toString());
-
-        //Stack newStack = new Stack (5);
-        //System.out.println(reverseString("Hello"));
-
-        QueueCircular nq = new QueueCircular(4);
-        nq.add(55);
-        nq.add(43);
-        nq.add(334);
-        nq.add(232);
-        nq.add(32);
-        nq.add(67);
+        ArrayList<Integer> newList;
+        newList = new ArrayList<Integer>();
 
 
-
-
-        System.out.println(nq.remove());
-        System.out.println(nq.remove());
-        System.out.println(nq.remove());
-        System.out.println(nq.remove());
-        System.out.println(nq.remove());
-        System.out.println(nq.remove());
-
-        //System.out.println(nq.peekFront());
-    }
-
-
-    public static String reverseString (String str) {
-        int stackSize = str.length();
-        ReverseStringStack stroka = new ReverseStringStack(stackSize);
-        for (int i = 0; i < str.length(); i++) {
-            stroka.push(str.charAt(i));
-
-        }
-        String line = "";
-        while (! stroka.isEmpty()) {
-        line += stroka.pop();
-        }
-        return line;
     }
 
 }
